c264380a274cf7c411a6e43d90ecb0d4ba479345
Ornella1234 <ornynella25@gmail.com>
2024-03-21 10:51:52 +0100

    Twahinduye umukino

--- src/FillingAnArray.java
+++ src/FillingAnArray.java
@@ -2,16 +2,13 @@ public class FillingAnArray {
 
         public static void main(String[] args) {
 
-            int[] arr = new int[10];
+            int[] array = new int[10];
 
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] =  9-i;
+            for (int i = 0; i < array.length; i++) {
+                array[i] =  9-i;
             }
 
-            for (int value : arr) {
-
-                System.out.println(value);
-            }
+            for (int value : array) System.out.println(value);
         }
     }
 
